de36cfc905d1dbe1d84b9125d4b319314cf417bb
plyfager <yangyfaker@gmail.com>
2021-04-28 13:14:50 +0800

    complete unittest

--- tests/test_modules/test_lsgan_archs.py
+++ tests/test_modules/test_lsgan_archs.py
@@ -19,6 +19,8 @@ class TestLSGANGenerator(object):
         assert isinstance(g, LSGANGenerator)
         x = g(None, num_batches=3)
         assert x.shape == (3, 3, 128, 128)
+        x = g(None, num_batches=3, return_noise=True)
+        assert x['noise_batch'].shape == (3, 128)
 
         # test different output_scale
         config = dict(type='LSGANGenerator', noise_size=128, output_scale=64)
@@ -35,6 +37,8 @@ class TestLSGANGenerator(object):
         assert isinstance(g, LSGANGenerator)
         x = g(None, num_batches=3)
         assert x.shape == (3, 3, 128, 128)
+        x = g(None, num_batches=3, return_noise=True)
+        assert x['noise_batch'].shape == (3, 128)
 
         # test different output_scale
         config = dict(type='LSGANGenerator', noise_size=128, output_scale=64)
@@ -68,6 +72,18 @@ class TestLSGANDiscriminator(object):
         score = d(x)
         assert score.shape == (2, 1)
 
+        # test different config
+        config = dict(
+            type='LSGANDiscriminator',
+            in_channels=3,
+            input_scale=64,
+            out_act_cfg=dict(type='Sigmoid'))
+        d = build_module(config)
+        assert isinstance(d, LSGANDiscriminator)
+        x = torch.randn((2, 3, 64, 64))
+        score = d(x)
+        assert score.shape == (2, 1)
+
     @pytest.mark.skipif(not torch.cuda.is_available(), reason='requires cuda')
     def test_lsgan_discriminator_cuda(self):
 
@@ -79,6 +95,18 @@ class TestLSGANDiscriminator(object):
 
         # test different input_scale
         config = dict(type='LSGANDiscriminator', in_channels=3, input_scale=64)
+        d = build_module(config).cuda()
+        assert isinstance(d, LSGANDiscriminator)
+        x = torch.randn((2, 3, 64, 64))
+        score = d(x.cuda())
+        assert score.shape == (2, 1)
+
+        # test different config
+        config = dict(
+            type='LSGANDiscriminator',
+            in_channels=3,
+            input_scale=64,
+            out_act_cfg=dict(type='Sigmoid'))
         d = build_module(config).cuda()
         assert isinstance(d, LSGANDiscriminator)
         x = torch.randn((2, 3, 64, 64))
