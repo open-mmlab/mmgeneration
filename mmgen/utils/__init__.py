04b36c15de0685dadd63bf9fd85c07a29b9f28b3
nbei <nbeiray@gmail.com>
2021-06-11 20:44:12 +0800

    add function of downloading from url

--- mmgen/utils/__init__.py
+++ mmgen/utils/__init__.py
@@ -1,4 +1,5 @@
 from .collect_env import collect_env
+from .io_utils import download_from_url
 from .logger import get_root_logger
 
-__all__ = ['collect_env', 'get_root_logger']
+__all__ = ['collect_env', 'get_root_logger', 'download_from_url']
