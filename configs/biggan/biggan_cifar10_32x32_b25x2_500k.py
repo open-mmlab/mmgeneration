1e66eff5237093b0762ab4cc522e8910ca5176ae
yangyifei <PJLAB\yangyifei@shai14001042l.pjlab.org>
2021-07-07 18:10:45 +0800

    support training for bigGAN on cifar-10

--- configs/biggan/biggan_cifar10_32x32_b25x2_500k.py
+++ configs/biggan/biggan_cifar10_32x32_b25x2_500k.py
@@ -5,8 +5,11 @@ _base_ = [
 
 # define dataset
 # you must set `samples_per_gpu`
-data = dict(samples_per_gpu=50, workers_per_gpu=8)
+data = dict(samples_per_gpu=25, workers_per_gpu=8)
 
+# adjust running config
+lr_config = None
+checkpoint_config = dict(interval=5000, by_epoch=False, max_keep_ckpts=20)
 custom_hooks = [
     dict(
         type='VisualizeUnconditionalSamples',
@@ -17,14 +20,10 @@ custom_hooks = [
         module_keys=('generator_ema', ),
         interval=4,
         start_iter=4000,
+        interp_cfg=dict(momentum=0.9999),
         priority='VERY_HIGH')
 ]
 
-# adjust running config
-lr_config = None
-checkpoint_config = dict(interval=5000, by_epoch=False, max_keep_ckpts=20)
-
-# total_iters = 250000
 total_iters = 500000
 
 # use ddp wrapper for faster training
@@ -36,8 +35,8 @@ runner = dict(
     is_dynamic_ddp=False,  # Note that this flag should be False.
     pass_training_status=True)
 
-inception_pkl = './work_dirs/inception_pkl/cifar10.pkl'
-
+# Note set your inception_pkl's path
+inception_pkl = None
 evaluation = dict(
     type='GenerativeEvalHook',
     interval=10000,
@@ -49,7 +48,8 @@ evaluation = dict(
             bgr2rgb=True),
         dict(type='IS', num_images=50000)
     ],
-    sample_kwargs=dict(sample_model='ema'))
+    sample_kwargs=dict(sample_model='ema'),
+    best_metric=['fid', 'is'])
 
 metrics = dict(
     fid50k=dict(
