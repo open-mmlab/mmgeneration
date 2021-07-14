da2e28a6915da52bb8ba31ba9d5b6f6b074a15bd
yangyifei <PJLAB\yangyifei@shai14001042l.pjlab.org>
2021-07-14 11:38:30 +0800

    support biggan-deep model

--- mmgen/models/architectures/biggan/modules.py
+++ mmgen/models/architectures/biggan/modules.py
@@ -94,7 +94,7 @@ class BigGANGenResBlock(nn.Module):
                 kernel_size=1,
                 stride=1,
                 padding=0,
-                conv_cfg=conv_cfg,
+                conv_cfg=shortcut_cfg,
                 act_cfg=None,
                 with_spectral_norm=with_spectral_norm,
                 spectral_norm_cfg=dict(eps=sn_eps))
@@ -364,7 +364,7 @@ class BigGANDiscResBlock(nn.Module):
                 kernel_size=1,
                 stride=1,
                 padding=0,
-                conv_cfg=conv_cfg,
+                conv_cfg=shortcut_cfg,
                 act_cfg=None,
                 with_spectral_norm=with_spectral_norm,
                 spectral_norm_cfg=dict(eps=sn_eps))
@@ -430,5 +430,312 @@ class BigGANDiscResBlock(nn.Module):
         x0 = self.conv2(x0)
         if self.with_downsample:
             x0 = self.downsample(x0)
+        x1 = self.forward_sc(x)
+        return x0 + x1
+
+
+@MODULES.register_module()
+class BigGANDeepGenResBlock(nn.Module):
+    """Residual block used in BigGAN's generator.
+
+    Args:
+        in_channels (int): The channel number of the input feature map.
+        out_channels (int): The channel number of the output feature map.
+        dim_after_concat (int): The channel number of the noise concatenated
+            with the class vector.
+        conv_cfg (dict, optional): Config for the convolution module used in
+            this residual block. Defaults to dict(type='Conv2d').
+        act_cfg (dict, optional): Config for the activation layer. Defaults to
+            dict(type='ReLU').
+        upsample_cfg (dict, optional): Config for the upsampling operation.
+            Defaults to dict(type='nearest', scale_factor=2).
+        sn_eps (float, optional): Epsilon value for spectral normalization.
+            Defaults to 1e-6.
+        with_spectral_norm (bool, optional): Whether to use spectral
+            normalization in this block. Defaults to True.
+        input_is_label (bool, optional): Whether the input of BNs' linear layer
+            is raw label. Defaults to False.
+        auto_sync_bn (bool, optional): Whether to use synchronized batch
+            normalization. Defaults to True.
+    """
+
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 dim_after_concat,
+                 conv_cfg=dict(type='Conv2d'),
+                 act_cfg=dict(type='ReLU'),
+                 upsample_cfg=dict(type='nearest', scale_factor=2),
+                 sn_eps=1e-6,
+                 bn_eps=1e-5,
+                 with_spectral_norm=True,
+                 input_is_label=False,
+                 auto_sync_bn=True,
+                 channel_ratio=4):
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.hidden_channels = self.in_channels // channel_ratio
+        self.activation = build_activation_layer(act_cfg)
+        self.upsample_cfg = deepcopy(upsample_cfg)
+        self.with_upsample = upsample_cfg is not None
+        if self.with_upsample:
+            self.upsample_layer = build_upsample_layer(self.upsample_cfg)
+        # Here in_channels of BigGANGenResBlock equal to num_features of
+        # BigGANConditionBN
+        self.bn1 = BigGANConditionBN(
+            in_channels,
+            dim_after_concat,
+            sn_eps=sn_eps,
+            bn_eps=bn_eps,
+            input_is_label=input_is_label,
+            with_spectral_norm=with_spectral_norm,
+            auto_sync_bn=auto_sync_bn)
+        # Here out_channels of BigGANGenResBlock equal to num_features of
+        # BigGANConditionBN
+        self.bn2 = BigGANConditionBN(
+            self.hidden_channels,
+            dim_after_concat,
+            sn_eps=sn_eps,
+            bn_eps=bn_eps,
+            input_is_label=input_is_label,
+            with_spectral_norm=with_spectral_norm,
+            auto_sync_bn=auto_sync_bn)
+
+        self.bn3 = BigGANConditionBN(
+            self.hidden_channels,
+            dim_after_concat,
+            sn_eps=sn_eps,
+            bn_eps=bn_eps,
+            input_is_label=input_is_label,
+            with_spectral_norm=with_spectral_norm,
+            auto_sync_bn=auto_sync_bn)
+
+        self.bn4 = BigGANConditionBN(
+            self.hidden_channels,
+            dim_after_concat,
+            sn_eps=sn_eps,
+            bn_eps=bn_eps,
+            input_is_label=input_is_label,
+            with_spectral_norm=with_spectral_norm,
+            auto_sync_bn=auto_sync_bn)
+
+        self.conv1 = SNConvModule(
+            in_channels=in_channels,
+            out_channels=self.hidden_channels,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            conv_cfg=conv_cfg,
+            act_cfg=None,
+            with_spectral_norm=with_spectral_norm,
+            spectral_norm_cfg=dict(eps=sn_eps))
+
+        self.conv2 = SNConvModule(
+            in_channels=self.hidden_channels,
+            out_channels=self.hidden_channels,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            conv_cfg=conv_cfg,
+            act_cfg=None,
+            with_spectral_norm=with_spectral_norm,
+            spectral_norm_cfg=dict(eps=sn_eps))
+
+        self.conv3 = SNConvModule(
+            in_channels=self.hidden_channels,
+            out_channels=self.hidden_channels,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            conv_cfg=conv_cfg,
+            act_cfg=None,
+            with_spectral_norm=with_spectral_norm,
+            spectral_norm_cfg=dict(eps=sn_eps))
+
+        self.conv4 = SNConvModule(
+            in_channels=self.hidden_channels,
+            out_channels=out_channels,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            conv_cfg=conv_cfg,
+            act_cfg=None,
+            with_spectral_norm=with_spectral_norm,
+            spectral_norm_cfg=dict(eps=sn_eps))
+
+    def forward(self, x, y):
+        """Forward function.
+
+        Args:
+            x (torch.Tensor): Input feature map tensor.
+            y (torch.Tensor): Label tensor or class embedding concatenated with
+                noise tensor.
+
+        Returns:
+            torch.Tensor: Output feature map tensor.
+        """
+        x0 = self.bn1(x, y)
+        x0 = self.activation(x0)
+        x0 = self.conv1(x0)
+
+        x0 = self.bn2(x0, y)
+        x0 = self.activation(x0)
+        # Drop channels in x  if necessary
+        if self.in_channels != self.out_channels:
+            x = x[:, :self.out_channels]
+        # unsample both h and x at this point
+        if self.with_upsample:
+            x0 = self.upsample_layer(x0)
+            x = self.upsample_layer(x)
+        x0 = self.conv2(x0)
+
+        x0 = self.bn3(x0, y)
+        x0 = self.activation(x0)
+        x0 = self.conv3(x0)
+
+        x0 = self.bn4(x0, y)
+        x0 = self.activation(x0)
+        x0 = self.conv4(x0)
+        return x0 + x
+
+
+@MODULES.register_module()
+class BigGANDeepDiscResBlock(nn.Module):
+    """Residual block used in BigGAN's discriminator.
+
+    Args:
+        in_channels (int): The channel number of the input tensor.
+        out_channels (int): The channel number of the output tensor.
+        conv_cfg (dict, optional): Config for the convolution module used in
+            this block. Defaults to dict(type='Conv2d').
+        shortcut_cfg (dict, optional): Config for the convolution module used
+            in shortcut. Defaults to dict(type='Conv2d').
+        act_cfg (dict, optional): Config for the activation layer. Defaults to
+            dict(type='ReLU', inplace=False).
+        sn_eps (float, optional): Epsilon value for spectral normalization.
+            Defaults to 1e-6.
+        with_downsample (bool, optional): Whether to use downsampling in this
+            block. Defaults to True.
+        with_spectral_norm (bool, optional): Whether to use spectral
+            normalization. Defaults to True.
+        is_head_block (bool, optional): Whether this block is the first block
+            of BigGAN. Defaults to False.
+    """
+
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 channel_ratio=4,
+                 conv_cfg=dict(type='Conv2d'),
+                 shortcut_cfg=dict(type='Conv2d'),
+                 act_cfg=dict(type='ReLU', inplace=False),
+                 sn_eps=1e-6,
+                 with_downsample=True,
+                 with_spectral_norm=True):
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.hidden_channels = self.out_channels // channel_ratio
+        self.activation = build_activation_layer(act_cfg)
+        self.with_downsample = with_downsample
+
+        if self.with_downsample:
+            self.downsample = nn.AvgPool2d(kernel_size=2, stride=2)
+
+        self.learnable_sc = (in_channels != out_channels)
+        if self.learnable_sc:
+            self.shortcut = SNConvModule(
+                in_channels=in_channels,
+                out_channels=out_channels - in_channels,
+                kernel_size=1,
+                stride=1,
+                padding=0,
+                conv_cfg=shortcut_cfg,
+                act_cfg=None,
+                with_spectral_norm=with_spectral_norm,
+                spectral_norm_cfg=dict(eps=sn_eps))
+
+        self.conv1 = SNConvModule(
+            in_channels=in_channels,
+            out_channels=self.hidden_channels,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            conv_cfg=conv_cfg,
+            act_cfg=act_cfg,
+            with_spectral_norm=with_spectral_norm,
+            spectral_norm_cfg=dict(eps=sn_eps),
+            order=('act', 'conv', 'norm'))
+
+        self.conv2 = SNConvModule(
+            in_channels=self.hidden_channels,
+            out_channels=self.hidden_channels,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            conv_cfg=conv_cfg,
+            act_cfg=act_cfg,
+            with_spectral_norm=with_spectral_norm,
+            spectral_norm_cfg=dict(eps=sn_eps),
+            order=('act', 'conv', 'norm'))
+
+        self.conv3 = SNConvModule(
+            in_channels=self.hidden_channels,
+            out_channels=self.hidden_channels,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            conv_cfg=conv_cfg,
+            act_cfg=act_cfg,
+            with_spectral_norm=with_spectral_norm,
+            spectral_norm_cfg=dict(eps=sn_eps),
+            order=('act', 'conv', 'norm'))
+
+        self.conv4 = SNConvModule(
+            in_channels=self.hidden_channels,
+            out_channels=out_channels,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            conv_cfg=conv_cfg,
+            act_cfg=None,
+            with_spectral_norm=with_spectral_norm,
+            spectral_norm_cfg=dict(eps=sn_eps))
+
+    def forward_sc(self, x):
+        """Forward function of shortcut.
+
+        Args:
+            x (torch.Tensor): Input feature map tensor.
+
+        Returns:
+            torch.Tensor: Output tensor of shortcut.
+        """
+        if self.with_downsample:
+            x = self.downsample(x)
+        if self.learnable_sc:
+            x0 = self.shortcut(x)
+            x = torch.cat([x, x0], dim=1)
+        return x
+
+    def forward(self, x):
+        """Forward function.
+
+        Args:
+            x (torch.Tensor): Input feature map tensor.
+
+        Returns:
+            torch.Tensor: Output feature map tensor.
+        """
+
+        x0 = self.conv1(x)
+        x0 = self.conv2(x0)
+        x0 = self.conv3(x0)
+        x0 = self.activation(x0)
+        # downsample
+        if self.with_downsample:
+            x0 = self.downsample(x0)
+        x0 = self.conv4(x0)
         x1 = self.forward_sc(x)
         return x0 + x1
