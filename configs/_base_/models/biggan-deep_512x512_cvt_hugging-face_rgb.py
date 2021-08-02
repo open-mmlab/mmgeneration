12ca0ad34f99cdce1fd267afc482ce22d5574ad2
Yifei Yang <yangyfaker@gmail.com>
2021-08-02 13:24:16 +0800

    [Feature] Release a benchmark for BigGAN/BigGAN-Deep (#80)
    * Add BigGAN's benchmark
    
    * Modify benchmark and add more configs for users
    
    * fix lint
    
    * fix pr#80
    
    Co-authored-by: yangyifei <PJLAB\yangyifei@shai14001042l.pjlab.org>
    Co-authored-by: Yangyfaker <yyf@YangyfakerdeMacBook-Air.local>

---
+++ configs/_base_/models/biggan-deep_512x512_cvt_hugging-face_rgb.py
@@ -0,0 +1,33 @@
+model = dict(
+    type='BasiccGAN',
+    generator=dict(
+        type='BigGANDeepGenerator',
+        output_scale=512,
+        noise_size=128,
+        num_classes=1000,
+        base_channels=128,
+        shared_dim=128,
+        with_shared_embedding=True,
+        sn_eps=1e-6,
+        init_type='ortho',
+        act_cfg=dict(type='ReLU', inplace=True),
+        concat_noise=True,
+        auto_sync_bn=False,
+        rgb2bgr=True),
+    discriminator=dict(
+        type='BigGANDeepDiscriminator',
+        input_scale=512,
+        num_classes=1000,
+        base_channels=128,
+        sn_eps=1e-6,
+        init_type='ortho',
+        act_cfg=dict(type='ReLU', inplace=True),
+        with_spectral_norm=True),
+    gan_loss=dict(type='GANLoss', gan_type='hinge'))
+
+train_cfg = dict(
+    disc_steps=8, gen_steps=1, batch_accumulation_steps=8, use_ema=True)
+test_cfg = None
+optimizer = dict(
+    generator=dict(type='Adam', lr=0.0001, betas=(0.0, 0.999), eps=1e-6),
+    discriminator=dict(type='Adam', lr=0.0004, betas=(0.0, 0.999), eps=1e-6))
