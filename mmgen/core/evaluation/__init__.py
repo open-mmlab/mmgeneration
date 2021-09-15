0032271c7923dc5b414b25b7daf349db561a94d7
LeoXing1996 <xingzn1996@hotmail.com>
2021-09-15 12:35:58 +0800

    [Feature] Support KLD metric and support evaluation for probabilistic models (#108)
    * support KLD metric
    
    * add init under tests
    
    * solve import error
    
    * add manually type convert for pt<1.8
    
    * remove an invalid element from evaluation. init
    
    * fix by comment

--- mmgen/core/evaluation/__init__.py
+++ mmgen/core/evaluation/__init__.py
@@ -3,10 +3,21 @@ from .eval_hooks import GenerativeEvalHook
 from .evaluation import (make_metrics_table, make_vanilla_dataloader,
                          single_gpu_evaluation, single_gpu_online_evaluation)
 from .metric_utils import slerp
-from .metrics import IS, MS_SSIM, PR, SWD, ms_ssim, sliced_wasserstein
+from .metrics import (IS, MS_SSIM, PR, SWD, GaussianKLD, ms_ssim,
+                      sliced_wasserstein)
 
 __all__ = [
-    'MS_SSIM', 'SWD', 'ms_ssim', 'sliced_wasserstein', 'single_gpu_evaluation',
-    'single_gpu_online_evaluation', 'PR', 'IS', 'slerp', 'GenerativeEvalHook',
-    'make_metrics_table', 'make_vanilla_dataloader'
+    'MS_SSIM',
+    'SWD',
+    'ms_ssim',
+    'sliced_wasserstein',
+    'single_gpu_evaluation',
+    'single_gpu_online_evaluation',
+    'PR',
+    'IS',
+    'slerp',
+    'GenerativeEvalHook',
+    'make_metrics_table',
+    'make_vanilla_dataloader',
+    'GaussianKLD',
 ]
