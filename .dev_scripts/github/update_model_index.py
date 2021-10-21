d81c7fcb60f7b58846e2b7a833cc6d2c65ac05ac
ys-li <56712176+Yshuo-Li@users.noreply.github.com>
2021-10-21 11:10:16 +0800

    [Docs] Update 'In Collection' (#141)
    * [Docs] Update 'In Collection'
    
    * [Docs] Update 'In Collection'
    
    * [Docs] Update 'In Collection', exit(0)
    
    * [Docs] Update metafile

--- .dev_scripts/github/update_model_index.py
+++ .dev_scripts/github/update_model_index.py
@@ -136,6 +136,7 @@ def parse_md(md_file, task):
                 if 'ALGORITHM' in lines[i] or 'BACKBONE' in lines[i]:
                     collection['Metadata']['Architecture'].append(name)
                     collection['Name'] = name
+                    collection_name = name
                 # get paper url
                 collection['Paper'].append(url)
                 i = j + 1
@@ -287,4 +288,4 @@ if __name__ == '__main__':
 
     file_modified |= update_model_index()
 
-    sys.exit(1 if file_modified else 0)
+    sys.exit(0)
