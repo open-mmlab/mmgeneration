716da33574c6f7ec49783bf44cc49e73bed13ef9
nbei <nbeiray@gmail.com>
2021-10-23 14:51:30 +0800

    revise running command

--- .dev_scripts/regression_benchmark/test_benchmark.sh
+++ .dev_scripts/regression_benchmark/test_benchmark.sh
@@ -3,6 +3,6 @@ CHECKPOINT_DIR=$2
 
 echo 'configs/styleganv2/stylegan2_c2_ffhq_256_b4x8_800k.py' &
 # GPUS=1 for the test with data parallel
-GPUS=1 tools/slurm_eval.sh ${PARTITION} test-benchmark \
+GPUS=1 bash tools/slurm_eval.sh ${PARTITION} test-benchmark \
     configs/styleganv2/stylegan2_c2_ffhq_256_b4x8_800k.py \
     ${CHECKPOINT_DIR}/styleganv2/stylegan2/stylegan2_c2_ffhq_256_b4x8_20210407_160709-7890ae1f.pth &
