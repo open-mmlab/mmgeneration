e85c7b4087324fa3d7d02aee8b317b8275e43012
nbei <nbeiray@gmail.com>
2021-10-23 14:57:21 +0800

    fix bug in ckpt path

--- .dev_scripts/regression_benchmark/test_benchmark.sh
+++ .dev_scripts/regression_benchmark/test_benchmark.sh
@@ -5,4 +5,4 @@ echo 'configs/styleganv2/stylegan2_c2_ffhq_256_b4x8_800k.py' &
 # GPUS=1 for the test with data parallel
 GPUS=1 bash tools/slurm_eval.sh ${PARTITION} test-benchmark \
     configs/styleganv2/stylegan2_c2_ffhq_256_b4x8_800k.py \
-    ${CHECKPOINT_DIR}/styleganv2/stylegan2/stylegan2_c2_ffhq_256_b4x8_20210407_160709-7890ae1f.pth &
+    ${CHECKPOINT_DIR}/styleganv2/stylegan2_c2_ffhq_256_b4x8_20210407_160709-7890ae1f.pth &
