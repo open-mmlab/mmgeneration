3381547520750c6bdbf4b5d0b4052126d7ece14e
nbei <nbeiray@gmail.com>
2021-10-24 14:20:25 +0800

    add multiple test

--- .dev_scripts/regression_benchmark/test_benchmark.sh
+++ .dev_scripts/regression_benchmark/test_benchmark.sh
@@ -7,3 +7,10 @@ GPUS=1 bash tools/slurm_eval.sh ${PARTITION} test-benchmark \
     configs/styleganv2/stylegan2_c2_ffhq_256_b4x8_800k.py \
     ${CHECKPOINT_DIR}/stylegan2/stylegan2_c2_ffhq_256_b4x8_20210407_160709-7890ae1f.pth \
     --online &
+y
+echo 'configs/styleganv1/styleganv1_ffhq_256_g8_25Mimg.py' &
+# GPUS=1 for the test with data parallel
+GPUS=1 bash tools/slurm_eval.sh ${PARTITION} test-benchmark \
+    configs/styleganv1/styleganv1_ffhq_256_g8_25Mimg.py \
+    ${CHECKPOINT_DIR}/styleganv1/styleganv1_ffhq_1024_g8_25Mimg_20210407_161627-850a7234.pth \
+    --online &
