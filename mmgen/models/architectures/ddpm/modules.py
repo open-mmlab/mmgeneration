dc8574ba2bd89325482496c819bd0125b98ebffe
LeoXing <leoxing1996@gmail.com>
2021-11-08 20:56:41 +0800

    fix bug in pt version check

--- mmgen/models/architectures/ddpm/modules.py
+++ mmgen/models/architectures/ddpm/modules.py
@@ -42,8 +42,8 @@ class SiLU(nn.Module):
 
     def __init__(self, inplace=False):
         super().__init__()
-        if torch.version <= '1.5.0' and inplace:
-            mmcv.print_log(f'Inplace version of \'SiLU\' is not supported for '
+        if torch.__version__ <= '1.5.0' and inplace:
+            mmcv.print_log('Inplace version of \'SiLU\' is not supported for '
                            f'torch <= 1.5.0, found \'{torch.version}\'.')
         self.inplace = inplace
 
@@ -56,7 +56,7 @@ class SiLU(nn.Module):
             torch.Tensor: Tensor after activation.
         """
 
-        if torch.version <= '1.5.0':
+        if torch.__version__ <= '1.5.0':
             return x * torch.sigmoid(x)
 
         return F.silu(x, inplace=self.inplace)
