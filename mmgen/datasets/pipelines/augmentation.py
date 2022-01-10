1246986d6d5eb5c5c468ae6c3a5886c87915fe06
makecent <42603768+makecent@users.noreply.github.com>
2022-01-10 17:29:40 +0800

    Update augmentation.py

--- mmgen/datasets/pipelines/augmentation.py
+++ mmgen/datasets/pipelines/augmentation.py
@@ -169,7 +169,7 @@ class Resize:
         else:
             img, w_scale, h_scale = mmcv.imresize(
                 img,
-                scale[::-1],
+                scale,
                 return_scale=True,
                 interpolation=self.interpolation,
                 backend=self.backend)
@@ -205,7 +205,10 @@ class Resize:
                 scale = (self.scale[-1], int(self.scale[-1] / w * h))
         else:
             # direct use the given ones
-            scale = self.scale
+            if mmcv.is_tuple_of(self.scale, int):
+                scale = self.scale[::-1]
+            else:
+                scale = self.scale
 
         # here we assume all images in self.keys have the same input size
         for key in self.keys:
