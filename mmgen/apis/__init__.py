9f611bb2ccd22dd353ba56c287e15961ffa6d3db
Tommy in Tongji <36354458+TommyZihao@users.noreply.github.com>
2022-01-17 15:31:27 +0800

    Update __init__.py
    fix the typo error

--- mmgen/apis/__init__.py
+++ mmgen/apis/__init__.py
@@ -1,11 +1,11 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .inference import (init_model, sample_conditional_model,
                         sample_ddpm_model, sample_img2img_model,
-                        sample_uncoditional_model)
+                        sample_unconditional_model)
 from .train import set_random_seed, train_model
 
 __all__ = [
     'set_random_seed', 'train_model', 'init_model', 'sample_img2img_model',
-    'sample_uncoditional_model', 'sample_conditional_model',
+    'sample_unconditional_model', 'sample_conditional_model',
     'sample_ddpm_model'
 ]
