637656fc773da298071001d40445044e36f3cf1f
plyfager <2744335995@qq.com>
2022-05-17 02:45:35 +0000

    Refactor train test interface.

--- tools/test.py
+++ tools/test.py
@@ -12,10 +12,15 @@ from mmgen.datasets import *  # noqa: F401,F403
 from mmgen.models import *  # noqa: F401,F403
 
 
+# TODO: support fuse_conv_bn, visualization, and format_only
 def parse_args():
-    parser = argparse.ArgumentParser(description='Train a detector')
-    parser.add_argument('config', help='train config file path')
-    parser.add_argument('--work-dir', help='the dir to save logs and models')
+    parser = argparse.ArgumentParser(
+        description='MMDet test (and eval) a model')
+    parser.add_argument('config', help='test config file path')
+    parser.add_argument('checkpoint', help='checkpoint file')
+    parser.add_argument(
+        '--work-dir',
+        help='the directory to save the file containing evaluation metrics')
     parser.add_argument(
         '--cfg-options',
         nargs='+',
@@ -35,13 +40,13 @@ def parse_args():
     args = parser.parse_args()
     if 'LOCAL_RANK' not in os.environ:
         os.environ['LOCAL_RANK'] = str(args.local_rank)
-
     return args
 
 
 def main():
     args = parse_args()
 
+    # load config
     cfg = Config.fromfile(args.config)
     cfg.launcher = args.launcher
     if args.cfg_options is not None:
@@ -56,8 +61,13 @@ def main():
         cfg.work_dir = osp.join('./work_dirs',
                                 osp.splitext(osp.basename(args.config))[0])
 
+    cfg.load_from = args.checkpoint
+
+    # build the runner from config
     runner = Runner.from_cfg(cfg)
-    runner.train()
+
+    # start testing
+    runner.test()
 
 
 if __name__ == '__main__':
