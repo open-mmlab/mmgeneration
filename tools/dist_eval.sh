69527fc159fa13e772179b59f1fbef8671791e91
The Bao <59738868+thebao0273@users.noreply.github.com>
2022-05-27 00:10:58 +0700

    Update dist_eval.sh

--- tools/dist_eval.sh
+++ tools/dist_eval.sh
@@ -15,7 +15,7 @@ python -m torch.distributed.launch \
     --master_addr=$MASTER_ADDR \
     --nproc_per_node=$GPUS \
     --master_port=$PORT \
-    $(dirname "$0")/test.py \
+    $(dirname "$0")/evaluation.py \
     $CONFIG \
     $CHECKPOINT \
     --launcher pytorch \
