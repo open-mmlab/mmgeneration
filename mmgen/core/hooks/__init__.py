4579c61a26176c4e2e2344c3c13962d1dd2af6e8
leoxing1996 <xingzn1996@hotmail.com>
2022-06-24 13:39:09 +0000

    [Log] support log processor and timer for mmgen

--- mmgen/core/hooks/__init__.py
+++ mmgen/core/hooks/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .ceph_hooks import PetrelUploadHook
 from .ema_hook import ExponentialMovingAverageHook
+from .iter_time_hook import GenIterTimerHook
 from .pggan_fetch_data_hook import PGGANFetchDataHook
 from .pickle_data_hook import PickleDataHook
 from .visualization import VisualizationHook
@@ -9,5 +10,5 @@ from .visualize_training_samples import VisualizeUnconditionalSamples
 __all__ = [
     'VisualizeUnconditionalSamples', 'PGGANFetchDataHook',
     'ExponentialMovingAverageHook', 'VisualizationHook', 'PickleDataHook',
-    'PetrelUploadHook'
+    'PetrelUploadHook', 'GenIterTimerHook'
 ]
