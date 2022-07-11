219c4d29c3e20488a683556fca5c475a5c2209d7
LeoXing <leoxing1996@gmail.com>
2022-07-11 21:22:28 +0800

    revise s1 config

--- configs/_base_/models/stylegan/styleganv1_base.py
+++ configs/_base_/models/stylegan/styleganv1_base.py
@@ -5,24 +5,3 @@ model = dict(
     generator=dict(
         type='StyleGANv1Generator', out_size=None, style_channels=512),
     discriminator=dict(type='StyleGAN1Discriminator', in_size=None))
-
-train_cfg = dict(
-    use_ema=True,
-    transition_kimgs=600,
-    optimizer_cfg=dict(
-        generator=dict(type='Adam', lr=0.001, betas=(0.0, 0.99)),
-        discriminator=dict(type='Adam', lr=0.001, betas=(0.0, 0.99))),
-    g_lr_base=0.001,
-    d_lr_base=0.001,
-    g_lr_schedule=dict({
-        '128': 0.0015,
-        '256': 0.002,
-        '512': 0.003,
-        '1024': 0.003
-    }),
-    d_lr_schedule=dict({
-        '128': 0.0015,
-        '256': 0.002,
-        '512': 0.003,
-        '1024': 0.003
-    }))
