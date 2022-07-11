227aa9c62c256a008782ffa35f553081b6ae0e7f
LeoXing <leoxing1996@gmail.com>
2022-07-11 21:26:23 +0800

    revise init

--- mmgen/models/gans/__init__.py
+++ mmgen/models/gans/__init__.py
@@ -9,6 +9,7 @@ from .mspie_stylegan2 import MSPIEStyleGAN2
 from .progressive_growing_unconditional_gan import ProgressiveGrowingGAN
 from .sagan import SAGAN
 from .singan import PESinGAN, SinGAN
+from .stylegan1 import StyleGANv1
 from .stylegan2 import StyleGAN2
 from .stylegan3 import StyleGAN3
 from .wgan_gp import WGANGP
@@ -16,5 +17,6 @@ from .wgan_gp import WGANGP
 __all__ = [
     'BaseGAN', 'BaseConditionalGAN', 'ProgressiveGrowingGAN', 'SinGAN',
     'MSPIEStyleGAN2', 'PESinGAN', 'SAGAN', 'GANDataPreprocessor', 'LSGAN',
-    'StyleGAN2', 'BigGAN', 'StyleGAN3', 'DCGAN', 'WGANGP', 'GGAN', 'BigGAN'
+    'StyleGAN2', 'BigGAN', 'StyleGAN3', 'DCGAN', 'WGANGP', 'GGAN', 'BigGAN',
+    'StyleGANv1'
 ]
