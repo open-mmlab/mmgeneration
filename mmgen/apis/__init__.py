3230f84ef76bdac9429b56aa492d6e1f5865fdcb
Yifei Yang <2744335995@qq.com>
2022-08-22 16:01:20 +0800

    refactor tools, apps (#382)

--- mmgen/apis/__init__.py
+++ mmgen/apis/__init__.py
@@ -1,9 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .inference import (init_model, sample_conditional_model,
                         sample_ddpm_model, sample_img2img_model,
-                        sample_unconditional_model)
+                        sample_unconditional_model, set_random_seed)
 
 __all__ = [
     'init_model', 'sample_img2img_model', 'sample_unconditional_model',
-    'sample_conditional_model', 'sample_ddpm_model'
+    'sample_conditional_model', 'sample_ddpm_model', 'set_random_seed'
 ]
