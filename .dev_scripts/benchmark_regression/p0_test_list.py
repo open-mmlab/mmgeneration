dbb170869a608f147aa2d21d40cd36d92b5a453f
LeoXing1996 <xingzn1996@hotmail.com>
2022-08-23 17:45:56 +0800

    update config name in P0 list (#389)

--- .dev_scripts/benchmark_regression/p0_test_list.py
+++ .dev_scripts/benchmark_regression/p0_test_list.py
@@ -1,8 +1,9 @@
 model_list = [
-    'biggan_ajbrock-sn_imagenet1k_128x128_b32x8_1500k',
-    'cyclegan_lsgan_id0_resnet_in_facades_b1x1_80k',
-    'ddpm_cosine_hybird_timestep-4k_imagenet1k_64x64_b8x16_1500k',
-    'pix2pix_vanilla_unet_bn_facades_b1x1_80k',
-    'styleganv1_ffhq_1024_g8_25Mimg', 'stylegan2_c2_ffhq_1024_b4x8',
-    'stylegan3_t_noaug_fp16_gamma32.8_ffhq_1024_b4x8'
+    'biggan_ajbrock-sn_8xb32-1500kiters_imagenet1k-128x128',
+    'cyclegan_lsgan-id0-resnet-in_1xb1-80kiters_facades',
+    'ddpm_cosine-hybird-timestep-4k_16xb8-1500kiters_imagenet1k-64x64',
+    'pix2pix_vanilla-unet-bn_1xb1-80kiters_facades',
+    'styleganv1_ffhq-256x256_8xb4-25Mimgs',
+    'stylegan2_c2_8xb4_ffhq-1024x1024',
+    'stylegan3-t_gamma32.8_8xb4-fp16-noaug_ffhq-1024x1024',
 ]
