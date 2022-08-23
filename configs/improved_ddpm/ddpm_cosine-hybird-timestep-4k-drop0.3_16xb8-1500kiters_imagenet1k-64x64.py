438f6ec1846b14b1895c44d3eb7c3fd33cbced44
plyfager <2744335995@qq.com>
2022-08-23 16:02:46 +0800

    lint

--- configs/improved_ddpm/ddpm_cosine-hybird-timestep-4k-drop0.3_16xb8-1500kiters_imagenet1k-64x64.py
+++ configs/improved_ddpm/ddpm_cosine-hybird-timestep-4k-drop0.3_16xb8-1500kiters_imagenet1k-64x64.py
@@ -1,4 +1,6 @@
-_base_ = ['./ddpm_cosine-hybird-timestep-4k_16xb8-1500kiters_imagenet1k-64x64.py']
+_base_ = [
+    './ddpm_cosine-hybird-timestep-4k_16xb8-1500kiters_imagenet1k-64x64.py'
+]
 
 # MODEL
 # set dropout prob as 0.3
